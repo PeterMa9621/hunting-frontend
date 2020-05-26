0348965918a3cce71157e1b397bce5773dbfe5ea
JingyuanMa <jma4@ualberta.ca>
2020-05-26 16:16:03 -0600

    update ServiceLayer

--- src/services/NewsService.js
+++ src/services/NewsService.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import News from '../model/News'
 
-const url = 'http://127.0.0.1:8000/api/news/';
+const url = 'http://localhost:8000/api/news/';
 
 class NewsService {
     static getNews(id = null){
